3a140fd094a793b5e4efaddf6ea0fbea6a8b61cf
Máté Szendrő <szendro.mate@gmail.com>
2017-05-25 14:59:51 +0100

    Add missing quotation marks

--- functions/fish_remove_git_aliases.fish
+++ functions/fish_remove_git_aliases.fish
@@ -1,7 +1,7 @@
 function fish_remove_git_aliases
     set -l aliases_file (dirname (status -f))/__fastgit_aliases.fish
-    if not type -q __fastgit_remove_aliases; and test -f $aliases_file
-        source $aliases_file
+    if not type -q __fastgit_remove_aliases; and test -f "$aliases_file"
+        source "$aliases_file"
     end
     if type -q __fastgit_remove_aliases
         __fastgit_remove_aliases
