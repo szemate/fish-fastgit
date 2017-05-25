dcd2c33230eb0bd235fa2fe1eb9bc89260262255
Máté Szendrő <szendro.mate@gmail.com>
2017-05-25 12:54:44 +0100

    Don't attempt to source file if generation fails

--- functions/fish_git_global_commands.fish
+++ functions/fish_git_global_commands.fish
@@ -1,7 +1,7 @@
 function fish_git_global_commands
     set -l aliases_file (dirname (status -f))/__fastgit_aliases.fish
     if not test -f "$aliases_file"
-        __fastgit_generate_aliases
+        __fastgit_generate_aliases; or exit 1
     end
     set -g __fastgit_global_commands 'true'
     source "$aliases_file"
--- functions/fish_update_git_aliases.fish
+++ functions/fish_update_git_aliases.fish
@@ -1,4 +1,4 @@
 function fish_update_git_aliases
-    __fastgit_generate_aliases
-    source (dirname (status -f))/__fastgit_aliases.fish
+    __fastgit_generate_aliases; and \
+        source (dirname (status -f))/__fastgit_aliases.fish
 end
