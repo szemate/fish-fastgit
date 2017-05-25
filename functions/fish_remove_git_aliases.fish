54d205a015db16d5c56f0096f706a189a1268402
Máté Szendrő <szendro.mate@gmail.com>
2017-05-25 17:02:21 +0100

    Don't unnecessarily source the aliases file

--- functions/fish_remove_git_aliases.fish
+++ functions/fish_remove_git_aliases.fish
@@ -1,8 +1,4 @@
 function fish_remove_git_aliases
-    set -l aliases_file (dirname (status -f))/__fastgit_aliases.fish
-    if not type -q __fastgit_remove_aliases; and test -f "$aliases_file"
-        source "$aliases_file"
-    end
     if type -q __fastgit_remove_aliases
         __fastgit_remove_aliases
     end
